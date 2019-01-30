11b14a1965c1ed6b95f94b5e4fba395273c75119
Luke Halley <luke123halley@gmail.com>
2019-01-30 12:55:45 +1000

    Document fishing mechanics and catch probability

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -3,6 +3,7 @@
 """Fishing quest system with catch mechanics and rewards."""
 QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing spot quality affects catch rate and rarity distribution
 # TODO: Add async support for better performance
+"""Fishing mechanics with catch probability based on rod quality, location, and player skill level."""
 # Enhancement: improve error messages
 # Note: Consider adding type annotations
 # Refactor: simplify control flow
