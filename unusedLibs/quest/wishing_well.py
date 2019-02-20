03f06d2b98dbaf8213245d0e1e1592f77ac2dbd7
Luke Halley <luke123halley@gmail.com>
2019-02-20 13:03:13 +1000

    Add module docstring for wishing well system

--- unusedLibs/quest/wishing_well.py
+++ unusedLibs/quest/wishing_well.py
@@ -1,3 +1,4 @@
+"""Wishing well system for rare item acquisition."""
 """Wishing well implementation with probability-based outcome generation."""
 """Wishing well mechanics including wish granting, probability tiers, and resource costs."""
 from web3 import Web3
