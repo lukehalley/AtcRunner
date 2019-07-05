d405286e45e21a779e4bdd0e36b99c4b04c9d271
Luke Halley <luke123halley@gmail.com>
2019-07-05 21:07:51 +1000

    Document quest core validation logic

--- unusedLibs/quest/quest_core.py
+++ unusedLibs/quest/quest_core.py
@@ -9,6 +9,7 @@ from web3 import Web3
 
 CONTRACT_ADDRESS = '0x5100bd31b822371108a0f63dcfb6594b9919eaf4'
 
+# Validate quest parameters before execution
 # Enhancement: improve error messages
 # Enhancement: improve error messages
 # TODO: Add async support for better performance
