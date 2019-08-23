7a7016e53318e1e721b35540e73511d9ffea9572
Luke Halley <luke123halley@gmail.com>
2019-08-23 13:45:49 +1000

    Add TODO for gene science improvements

--- unusedLibs/genes/gene_science_v2.py
+++ unusedLibs/genes/gene_science_v2.py
@@ -30,3 +30,4 @@ def mix_genes(genes1, genes2, block_number, crystal_id, rpc_address):
 # TODO: Optimize mutation probability calculations for better performance
 # TODO: Implement early termination for convergent genetic traits
 # Calculate mutation chance based on genetic stability
+# TODO: Refactor trait inheritance calculations for better performance
