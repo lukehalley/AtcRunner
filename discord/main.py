def04d9868ebd978664201739a981225052fcefe
Luke Halley <luke123halley@gmail.com>
2019-11-01 14:08:28 +1000

    Add module header documentation

--- discord/main.py
+++ discord/main.py
@@ -1,3 +1,4 @@
+# Discord bot main entry point
 """Main Discord bot handler for AtcRunner game integration."""
 # Initialize Discord bot client for command handling
 """Discord bot main module for handling bot operations and event listeners."""
