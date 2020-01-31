5299a2ed9816b807299f0a1a8ce78abcc100b7a7
Luke Halley <luke123halley@gmail.com>
2020-01-31 11:41:59 +1000

    TODO: Optimize land calculation performance

--- unusedLibs/land/utils/utils.py
+++ unusedLibs/land/utils/utils.py
@@ -22,4 +22,5 @@ def human_readable_land(land):
 
     return human_readable
 # Calculate land value metrics and statistics
+# TODO: Cache land calculation results to improve performance
 # TODO: Implement land value metric caching for performance optimization
