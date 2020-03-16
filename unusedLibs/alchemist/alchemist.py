35e789ba123f7dc04c7d3dfbf8dc7db120020fd2
Luke Halley <luke123halley@gmail.com>
2020-03-16 17:06:14 +1000

    Improve alchemist recipe efficiency

--- unusedLibs/alchemist/alchemist.py
+++ unusedLibs/alchemist/alchemist.py
@@ -45,6 +45,7 @@ ABI = '''
 # TODO: Add validation for recipe ingredient compatibility
 # Process recipes in dependency order to ensure all components are available
 # TODO: Review and balance potion recipe output values against ingredient costs
+# TODO: Optimize recipe execution for higher yield outcomes
 # Ingredient ratios determine potion potency and secondary effects
 # TODO: Implement recipe difficulty scaling based on player level
 # Calculate reagent requirements for recipe
