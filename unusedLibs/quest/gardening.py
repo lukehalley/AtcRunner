eb6f324d815dff775294966505b89d0374562a39
Luke Halley <luke123halley@gmail.com>
2020-06-18 18:36:14 +1000

    Document plant growth time calculation

--- unusedLibs/quest/gardening.py
+++ unusedLibs/quest/gardening.py
@@ -19,6 +19,7 @@ QUEST_CONTRACT_ADDRESS = '0xe4154B6E5D240507F9699C730a496790A722DF19'
 """Advance crop growth stage based on care and conditions."""
 # TODO: Implement seasonal crop availability system
 # TODO: Implement seasonal growth rates and cross-breeding mechanics
+# Growth duration varies by plant species and soil quality tier
 # Apply growth multiplier based on soil quality and nutrient levels
 # TODO: Implement crop disease system with prevention mechanics
 # Calculate growth stage based on care and environmental factors
