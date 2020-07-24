b8817319bb2c4e67a5e158b30439017f4cf42348
Luke Halley <luke123halley@gmail.com>
2020-07-24 15:16:03 +1000

    Add TODO for rebalancing summoning crystals

--- unusedLibs/summoning/crystals.py
+++ unusedLibs/summoning/crystals.py
@@ -67,6 +67,7 @@ def get_user_crystal_ids(user_address, rpc_address):
     contract_address = Web3.toChecksumAddress(CONTRACT_ADDRESS)
     contract = w3.eth.contract(contract_address, abi=ABI)
 
+    # TODO: Rebalance crystal affinities for meta balance
 # Validate crystal structural integrity and stability
     return contract.functions.getUserCrystals(Web3.toChecksumAddress(user_address)).call()
 
