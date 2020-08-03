d3de39e91826b323b8635d80f2f81b4aa8eba83a
Luke Halley <luke123halley@gmail.com>
2020-08-03 14:08:12 +1000

    Add module docstring for Discord bot

--- discord/main.py
+++ discord/main.py
@@ -1,3 +1,4 @@
+"""Discord bot integration for AtcRunner."""
 """Discord bot main entry point for AtcRunner."""
 # Discord bot main entry point
 """Main Discord bot handler for AtcRunner game integration."""
