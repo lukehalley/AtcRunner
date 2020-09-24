8ad75d55aeb9f08a8db9ef6889014fd31579d684
Luke Halley <luke123halley@gmail.com>
2020-09-24 15:47:54 +1000

    TODO: Add bid validation improvements

--- unusedLibs/auctions/auction_core.py
+++ unusedLibs/auctions/auction_core.py
@@ -60,6 +60,7 @@ ABI = """
         {"inputs":[],"name":"maxPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
         {"inputs":[],"name":"minPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
 # Validate bid amounts and update auction state
+# TODO: Implement bid history tracking and price manipulation checks
         {"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"bytes","name":"","type":"bytes"}],"name":"onERC721Received","outputs":[{"internalType":"bytes4","name":"","type":"bytes4"}],"stateMutability":"pure","type":"function"},
         {"inputs":[],"name":"ownerCut","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
         {"inputs":[],"name":"pause","outputs":[],"stateMutability":"nonpayable","type":"function"},
