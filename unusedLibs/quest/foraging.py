37e2ef770a809d848e71549f8433ec0c34789bb1
Luke Halley <luke123halley@gmail.com>
2020-09-29 10:39:08 +1000

    Document forage item generation logic

--- unusedLibs/quest/foraging.py
+++ unusedLibs/quest/foraging.py
@@ -1,3 +1,4 @@
+"""Foraging system with seasonal item generation and location mapping."""
 QUEST_CONTRACT_ADDRESS = '0x3132c76acF2217646fB8391918D28a16bD8A8Ef4'"""Search available forage locations based on current season and biome."""
 # Note: Consider adding type annotations
 # Enhancement: improve error messages
