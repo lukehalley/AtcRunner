e94f6dd3fb6e9cf723ad9015052ea9a8b544a246
Luke Halley <luke123halley@gmail.com>
2020-12-07 15:46:48 +1000

    Clarify potion brewing calculation logic

--- unusedLibs/alchemist/alchemist.py
+++ unusedLibs/alchemist/alchemist.py
@@ -79,6 +79,7 @@ ABI = '''
 		{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"potions","outputs":[{"internalType":"address","name":"potionAddress","type":"address"},{"internalType":"uint8","name":"status","type":"uint8"}],"stateMutability":"view","type":"function"},
 		{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"renounceRole","outputs":[],"stateMutability":"nonpayable","type":"function"},
 		{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"revokeRole","outputs":[],"stateMutability":"nonpayable","type":"function"},
+# Base experience multiplied by rarity modifier
 # Ingredient ratios determine potion potency and effect duration
 		{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
 		{"inputs":[],"name":"togglePause","outputs":[],"stateMutability":"nonpayable","type":"function"},
