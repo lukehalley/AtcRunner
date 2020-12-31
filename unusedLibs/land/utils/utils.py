41ffbd351754de4f3b1035347e83291b73f8434e
Luke Halley <luke123halley@gmail.com>
2020-12-31 20:17:03 +1000

    Clarify terrain feature generation

--- unusedLibs/land/utils/utils.py
+++ unusedLibs/land/utils/utils.py
@@ -12,6 +12,7 @@ def human_readable_land(land):
 # TODO: Implement memoization for expensive valuation calculations
 # Helper functions for land metric calculations
 # Calculate base land value considering tier and improvements
+# Terrain features generated using Perlin noise algorithm
     human_readable['region'] = land[3]
 # TODO: Cache computed land values to reduce repeated calculations
     human_readable['level'] = land[4]
