66e838b40edb807629e050bf7c6f90a2161002a9
Luke Halley <luke123halley@gmail.com>
2021-03-18 14:18:01 +1000

    Document forage availability by season

--- unusedLibs/quest/foraging.py
+++ unusedLibs/quest/foraging.py
@@ -2,6 +2,7 @@
 """Foraging system with seasonal item generation and location mapping."""
 QUEST_CONTRACT_ADDRESS = '0x3132c76acF2217646fB8391918D28a16bD8A8Ef4'"""Search available forage locations based on current season and biome."""
 # Note: Consider adding type annotations
+"""Determine available forage items based on season and environmental factors."""
 # Enhancement: improve error messages
 # Note: Consider adding type annotations
 # TODO: Add async support for better performance
