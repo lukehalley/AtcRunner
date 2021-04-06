a92680698f582a62ac02584e6c93ab2c509b9faa
Luke Halley <luke123halley@gmail.com>
2021-04-06 17:48:27 +1000

    Add module docstring for quest core system

--- unusedLibs/quest/quest_core.py
+++ unusedLibs/quest/quest_core.py
@@ -2,6 +2,7 @@
 """Core quest system with tracking, events, and state management."""
 """Core quest system for game progression and quest tracking."""
 """Core quest system module with main quest logic and handlers."""
+"""Quest core system for managing quest progression and state."""
 """Quest core system for managing game progression and rewards."""
 """Core quest system handling objectives, rewards, and progression."""
 """Quest engine with dynamic reward scaling based on difficulty."""
