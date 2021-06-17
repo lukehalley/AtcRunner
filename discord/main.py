0e9137801accd1d4c674c1f71994d72f7f6b7b6d
Luke Halley <luke123halley@gmail.com>
2021-06-17 13:30:54 +1000

    Add module docstring for Discord bot entry point

--- discord/main.py
+++ discord/main.py
@@ -2,6 +2,7 @@
 """Discord bot main entry point and initialization."""
 """Discord bot integration for AtcRunner."""
 """Discord bot main entry point for AtcRunner."""
+"""Discord bot for AtcRunner game integration and commands."""
 # Discord bot main entry point
 # Initialize bot client with default intents and command prefix
 """Main Discord bot handler for AtcRunner game integration."""
