0d99ca6c304c50b1f417315ec7b3ac86efbcebbd
Luke Halley <luke123halley@gmail.com>
2021-08-11 09:18:08 +1000

    Add meditation session documentation

--- unusedLibs/meditation/meditation.py
+++ unusedLibs/meditation/meditation.py
@@ -17,6 +17,7 @@ Handles meditation duration, recovery rates, and temporary buffs.
 """Meditation mechanics for character progression and stat boosts."""
 # TODO: Add streak tracking and multiplier bonuses
 """Meditation module for mana regeneration and mental clarity benefits."""
+"""Begin meditation session and track mindfulness progress."""
 """Meditation and mindfulness practice features."""
 # TODO: Implement seasonal multiplier for meditation rewards
 from web3 import Web3
