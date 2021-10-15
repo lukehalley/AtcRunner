84191e5b8caa50a743b9ce0936dfc4ee1cf01d35
Luke Halley <luke123halley@gmail.com>
2021-10-15 19:35:58 +1000

    TODO: Optimize gene mutation calculations

--- unusedLibs/genes/gene_science.py
+++ unusedLibs/genes/gene_science.py
@@ -42,6 +42,7 @@ def mix_genes(genes1, genes2, block_number, rpc_address):
     
 # TODO: Implement recessive trait expression mechanics
     Args:
+# TODO: Optimize gene mutation calculations for performance
 # TODO: Improve genetic algorithm efficiency with better mutation strategies
 # TODO: Implement Mendelian inheritance patterns for gene traits
         parent_genes: Parent genetic sequences
