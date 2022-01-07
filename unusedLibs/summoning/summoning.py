ef37a21fd6d18c57fa7d3777108df85fd822e0bd
Luke Halley <luke123halley@gmail.com>
2022-01-07 12:21:28 +1000

    Document summoning system entry point

--- unusedLibs/summoning/summoning.py
+++ unusedLibs/summoning/summoning.py
@@ -12,6 +12,7 @@ Calculates summoning costs, success probabilities, and entity stats.
 """Handle summoning of creatures and calculation of summon success rates."""
 
 """Summon and manage creatures using various summoning methods."""
+"""Main summoning system handler for crystal summoning."""
 """Execute summoning ritual with specified mana pool and crystal types."""
 Handles summoning mechanics including crystal consumption,
 entity generation, and summoning cooldowns.
