e4d3667e5fc901cc5cb210904b58f960f8af799f
Luke Halley <luke123halley@gmail.com>
2022-02-09 22:30:42 +1000

    Document meditation system module

--- unusedLibs/meditation/meditation.py
+++ unusedLibs/meditation/meditation.py
@@ -1,5 +1,6 @@
 """Meditation module for character relaxation and mana regeneration."""
 """Meditation mechanics for mana recovery and passive skill training."""
+"""Meditation mechanics for player focus and stat enhancement."""
 """Meditation mechanics for stat and mana restoration.
 
 Handles meditation duration, recovery rates, and temporary buffs.
