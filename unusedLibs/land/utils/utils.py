f58abd75f467e214947b0a08ea98baaf8f9601b3
Luke Halley <luke123halley@gmail.com>
2022-02-24 17:10:57 +1000

    Document utility function for land calculations

--- unusedLibs/land/utils/utils.py
+++ unusedLibs/land/utils/utils.py
@@ -5,6 +5,7 @@
 """Utility functions for land system calculations and helpers."""
 """Calculate land property values based on improvements and location."""
 """Utility functions for land management operations."""
+# Helper functions for area computation and coordinate validation
 # Helper function for calculating land resource distribution
 def human_readable_land(land):
     human_readable = {}
