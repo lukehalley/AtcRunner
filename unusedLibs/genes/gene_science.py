5716d92e26d914a24dd829d480935df02320851a
Luke Halley <luke123halley@gmail.com>
2022-03-18 09:08:39 +1000

    Document gene calculation logic

--- unusedLibs/genes/gene_science.py
+++ unusedLibs/genes/gene_science.py
@@ -1,3 +1,4 @@
+"""Gene science system for calculating inherited traits and mutations."""
 """Gene science module for breeding and trait inheritance mechanics."""
 """Gene science module for trait inheritance calculations."""
 """Genetic algorithm for trait inheritance and selective breeding mechanics."""
