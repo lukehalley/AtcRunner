c66a877efff36db2bcb548ca5297e5411db4b0bb
Luke Halley <luke123halley@gmail.com>
2022-04-13 18:40:12 +0100

    synapse bridge automation done

--- helpers/Selenium.py
+++ helpers/Selenium.py
@@ -10,6 +10,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 from pyvirtualdisplay import Display
+import json
+
+#  Load Web Element Selectors
+with open('./helpers/webElements.json') as json_file:
+    webElements = json.load(json_file)
+
+x = 1
+
+def findWebElement(selector, timeout=30):
+    return WebDriverWait(driver, timeout).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, selector))
+    )
 
 def is_docker():
     path = '/proc/self/cgroup'
@@ -32,6 +44,7 @@ def initBrowser():
         chrome_options.add_argument(f"user-data-dir=/Users/luke/Documents/chrome")
 
     chrome_options.add_argument("profile-directory=Profile 6")
+    chrome_options.add_argument("--start-maximized")
 
     if isDocker:
         driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)
@@ -56,9 +69,7 @@ def loginIntoMetamask(driver):
     openMetamaskTab(driver)
 
     print("Waiting for Metamask password input...")
-    passwordInput = WebDriverWait(driver, 30).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#password"))
-    )
+    passwordInput = findWebElement(os.environ.get("METAMASK_PASSWORDINPUT"))
     print("Password input located!", "\n")
 
     print("Filling in password...")
@@ -71,9 +82,7 @@ def loginIntoMetamask(driver):
     print("Enter key pressed!", "\n")
 
     print("Checking if logged in...")
-    WebDriverWait(driver, 30).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#app-content > div > div.main-container-wrapper > div > div > div > div.tabs"))
-    )
+    findWebElement(os.environ.get("METAMASK_TABS"))
     print("Metamask logged in!", "\n")
 
 def buildBridgeURL(inputToken, outputToken, chainId):
@@ -86,28 +95,75 @@ def buildBridgeURL(inputToken, outputToken, chainId):
 
     return synapseBridgeURL
 
-def synapseBridge(driver):
+def executeSynapseBridge(driver, inputToken, outputToken, chainId, amount):
+
+    print("Building Synapse Bridge URL...")
+    synapseBridgeBridgeURL = buildBridgeURL(inputToken, outputToken, chainId)
+    print(f"Synapse Bridge URL built: {synapseBridgeBridgeURL}", "\n")
 
     print("Opening Synapse Bridge...")
-    synapseBridgeBridgeURL = os.environ.get("SYNAPSE_BRIDGE_URL")
-    driver.get(f'chrome-extension://{mmExtString}/home.html')
-    print("Metamask opened!", "\n")
+    driver.get(synapseBridgeBridgeURL)
+    print("Synapse Bridge opened!", "\n")
+
+    print(f"Checking current {inputToken} GUI balance...")
+    synapseCurrentTokenBalance = findWebElement(os.environ.get("SYNAPSE_GUIBALANCE"))
+    synapseGUIBalance = synapseCurrentTokenBalance.text
+    print(f"{inputToken} GUI balance is currently {synapseGUIBalance}", "\n")
+
+    print("Waiting for Synapse input and output token field...")
+    inputTokenField = findWebElement(os.environ.get("SYNAPSE_INPUTFIELD"))
+    outputTokenField = findWebElement(os.environ.get("SYNAPSE_OUTPUTFIELD"))
+    print("Synapse input and output token fields located!", "\n")
+
+    print(f"Entering {amount} {inputToken} as the input token...")
+    inputTokenField.send_keys(amount)
+    print("Entered input tokens!", "\n")
+
+    time.sleep(5)
+
+    print(f"Checking how many {inputToken} we will get after bridging...")
+    synapseTokenOut = outputTokenField.get_attribute('value')
+    print(f"We will get {synapseTokenOut} {inputToken} after we bridge.", "\n")
+
+    print(f"Checking {inputToken} bridge fee...")
+    synapseFee = findWebElement(os.environ.get("SYNAPSE_FEE"))
+    synapseFeeAmount = synapseFee.text
+    print(f"Bridge fee is {synapseFeeAmount} {inputToken}", "\n")
+
+    print(f"Checking bridge slippage fee...")
+    synapseSlippage = findWebElement(os.environ.get("SYNAPSE_SLIPPAGE"))
+    synapseSlippageAmount = synapseSlippage.text
+
+    if "-" in synapseSlippageAmount:
+        print(f"Bridge has a negative slippage of {synapseSlippageAmount}", "\n")
+    else:
+        print(f"Bridge has a positive slippage of {synapseSlippageAmount}", "\n")
+
+    time.sleep(5)
+
+    print(f"Checking bridge button status")
+    synapseBridgeBtnText = findWebElement(os.environ.get("SYNAPSE_BRIDGE_BTN_TXT"))
+    synapseBridgeStatusText = synapseBridgeBtnText.text
+    print(f"Current bridge button status is '{synapseBridgeStatusText}'", "\n")
+
+    if synapseBridgeBtnText == "Bridge Token":
+        readyToBridge = True
+    else:
+        readyToBridge = False
+
+    print(f"Ready to bridge: {readyToBridge}")
 
 def switchMetamaskNetwork(driver, networkToSwitchTo):
 
     openMetamaskTab(driver)
 
     print("Locating Metamask switch dropdown...")
-    switchDropdown = WebDriverWait(driver, 30).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#app-content > div > div.app-header.app-header--back-drop > div > div.app-header__account-menu-container > div.app-header__network-component-wrapper > div"))
-    )
+    switchDropdown = findWebElement(os.environ.get("METAMASK_SWITCHDROPDOWN"))
     print("Metamask switch dropdown located!", "\n")
 
     print("Getting current network...")
-    switchDropdown = WebDriverWait(driver, 30).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#app-content > div > div.app-header.app-header--back-drop > div > div.app-header__account-menu-container > div.app-header__network-component-wrapper > div > span"))
-    )
-    currentNetwork = switchDropdown.text
+    mmCurrentNetwork = findWebElement(os.environ.get("METAMASK_CURRENTNETWORK"))
+    currentNetwork = mmCurrentNetwork.text
     print(f"Got current network: {currentNetwork}", "\n")
 
     if (currentNetwork == networkToSwitchTo):
@@ -119,24 +175,20 @@ def switchMetamaskNetwork(driver, networkToSwitchTo):
         print("Metamask switch dropdown Clicked!", "\n")
 
         print("Checking dropdown is open...")
-        switchDropdown = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "#app-content > div > div.menu-droppo-container.network-droppo > div > button"))
-        )
+        findWebElement(os.environ.get("METAMASK_ADDNETWORKBUTTON"))
         print("Metamask switch dropdown is open!", "\n")
 
         print("Getting network list element...")
-        switchDropdown = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "#app-content > div > div.menu-droppo-container.network-droppo > div > div.network-dropdown-list"))
-        )
+        findWebElement(os.environ.get("METAMASK_NETWORKLIST"))
         print("Got network list element!", "\n")
 
         print(f"Switching to {networkToSwitchTo}...")
-        networkListItem = driver.find_element_by_xpath("//span[text()='DFK Chain']")
+        networkListItem = driver.find_element_by_xpath(f"//span[text()='{networkToSwitchTo}']")
         networkListItem.click()
         print(f"Switched to {networkToSwitchTo}!", "\n")
 
 driver = initBrowser()
 loginIntoMetamask(driver)
-bridgeURL = buildBridgeURL("JEWEL", "JEWEL", "53935")
-switchMetamaskNetwork(driver, "DFK Chain")
+switchMetamaskNetwork(driver, "avalanchedfk")
+executeSynapseBridge(driver, "JEWEL", "JEWEL", "1666600000", "10")
 x = 1
