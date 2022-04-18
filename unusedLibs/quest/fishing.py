2d216943fbb9894b48c7aef092bf096344ab33e9
Luke Halley <luke123halley@gmail.com>
2022-04-18 18:50:01 +1000

    Document fishing system mechanics

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -3,6 +3,7 @@
 """Fishing quest mechanics with catch rates and spawn system."""
 """Fishing mechanics with catch rarity determination and player skill scaling."""
 """Fishing quest implementation with species rarity system."""
+"""Fish species management and catch calculations"""
 """Fishing quest system with catch mechanics and rewards."""
 QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing spot quality affects catch rate and rarity distribution
 # TODO: Add async support for better performance
