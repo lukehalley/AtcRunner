35eafe053317a997d22acda2537f6586f00ea85e
Luke Halley <luke123halley@gmail.com>
2022-08-15 12:40:52 +1000

    Document genetic breeding system

--- unusedLibs/genes/gene_science.py
+++ unusedLibs/genes/gene_science.py
@@ -1,3 +1,4 @@
+"""Genetic trait inheritance and breeding mechanics."""
 """Gene science system for calculating inherited traits and mutations."""
 """Gene science module for breeding and trait inheritance mechanics."""
 """Gene science module for trait inheritance calculations."""
