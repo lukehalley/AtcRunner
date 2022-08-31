e6ed551982f094871619a2d62ed161f608701512
Luke Halley <luke123halley@gmail.com>
2022-08-31 21:59:27 +1000

    Document improved genetic algorithm with mutations

--- unusedLibs/genes/gene_science_v2.py
+++ unusedLibs/genes/gene_science_v2.py
@@ -36,3 +36,4 @@ def mix_genes(genes1, genes2, block_number, crystal_id, rpc_address):
 # TODO: Refactor trait inheritance calculations for better performance
 # TODO: Refactor gene combination logic for performance optimization
 # TODO: Simplify dominant/recessive trait resolution algorithm
+# Enhanced breeding calculation with mutation support
