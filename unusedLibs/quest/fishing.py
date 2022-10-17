2b1657ddfdf197d2d9e94e4af87902b67f0f7ca4
Luke Halley <luke123halley@gmail.com>
2022-10-17 16:36:30 +1000

    Add module docstring for fishing system

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -7,6 +7,7 @@
 """Fishing quest system with catch mechanics and rewards."""
 QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing spot quality affects catch rate and rarity distribution
 # TODO: Add async support for better performance
+"""Fishing mechanics and fish species management."""
 # Higher fishing skill unlocks rarer fish species
 """Fishing mechanics with catch probability based on rod quality, location, and player skill level."""
 # Enhancement: improve error messages
