de18461f3fe32565d8729dd54bf0095b3ebbc30e
Luke Halley <luke123halley@gmail.com>
2022-11-09 14:52:03 +1000

    Add TODO for gene inheritance implementation

--- unusedLibs/genes/gene_science.py
+++ unusedLibs/genes/gene_science.py
@@ -52,6 +52,7 @@ def mix_genes(genes1, genes2, block_number, rpc_address):
     Args:
 # TODO: Optimize gene mutation calculations for performance
 # TODO: Improve genetic algorithm efficiency with better mutation strategies
+# TODO: Implement Mendelian inheritance patterns for trait combinations
 # TODO: Implement Mendelian inheritance patterns for gene traits
         parent_genes: Parent genetic sequences
         mutation_rate: Probability of random mutation
