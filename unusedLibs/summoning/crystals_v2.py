9123827372e5767724ba3e44f7816db96b6ff806
Luke Halley <luke123halley@gmail.com>
2023-01-26 22:02:41 +1000

    Document crystal v2 enhancements

--- unusedLibs/summoning/crystals_v2.py
+++ unusedLibs/summoning/crystals_v2.py
@@ -5,6 +5,7 @@
 from web3 import Web3
 
 CONTRACT_ADDRESS = '0xf4d3ae202c9ae516f7eb1db5aff19bf699a5e355'
+"""Enhanced crystal system with improved rarity distribution algorithm."""
 
 ABI = """
     [
