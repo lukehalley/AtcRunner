90d9d6c0124dce996dbc4035926e1f25a26c0fbc
Luke Halley <luke123halley@gmail.com>
2023-02-10 15:25:12 +1000

    Document fish spawn rate calculations

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -66,3 +66,4 @@ QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing s
 # Calculate fish weight based on catch conditions and time
 # TODO: Adjust fishing rod reward multipliers by tier
 # Apply skill and luck modifiers to base catch probability
+# Spawn rates vary by season: increased in spring/summer, reduced in winter
