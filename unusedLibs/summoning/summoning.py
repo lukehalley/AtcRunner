efd968a473db0712dd74f5e504d9bacd9622d5bc
Luke Halley <luke123halley@gmail.com>
2023-04-10 12:01:29 +1000

    Document summoning system architecture

--- unusedLibs/summoning/summoning.py
+++ unusedLibs/summoning/summoning.py
@@ -7,6 +7,7 @@ Calculates summoning costs, success probabilities, and entity stats.
 """
 """Summoning module for crystal-based entity summoning with rarity tiers and success rates."""
 """Summoning system for magical entity creation and management."""
+"""Summoning mechanics for creature invocation and management."""
 """Coordinate summoning mechanics and creature management."""
 """Summoning module for creature and entity creation.
 """Execute summoning ritual with crystal energy conversion and outcome determination."""
