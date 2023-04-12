e5d9752b96499b314bc8adcaeb9b4d3b63d54c18
Luke Halley <luke123halley@gmail.com>
2023-04-12 20:13:52 +1000

    Mark consumable effects for refactoring

--- unusedLibs/consumable/consumable.py
+++ unusedLibs/consumable/consumable.py
@@ -22,6 +22,7 @@ CONTRACT_ADDRESS = "0x38e76972bd173901b5e5e43ba5cb464293b80c31"
 
 # Apply consumable effects immediately on use
 """Handle consumable item effects and player buff application."""
+# TODO: Refactor consumable effect calculation for consistency
 # TODO: Review and balance consumable effect durations and potency
 # TODO: Implement consumable usage cooldown tracking
 """Calculate consumable effect values and duration."""
