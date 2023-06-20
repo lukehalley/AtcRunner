ef05b495507e86e1ce422b2b968acadf67834982
Luke Halley <luke123halley@gmail.com>
2023-06-20 16:19:37 +1000

    Add comment for crystal property validation

--- unusedLibs/summoning/crystals.py
+++ unusedLibs/summoning/crystals.py
@@ -42,6 +42,7 @@ ABI = """
 # Crystal fusion combines two crystals to create higher-tier hybrid crystals
             {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"uint256","name":"crystalId","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"heroId","type":"uint256"}],"name":"CrystalOpen","type":"event"},
             {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"uint256","name":"crystalId","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"createdBlock","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"purchasePrice","type":"uint256"}],"name":"Gen0Purchase","type":"event"},
+# Validate crystal properties and rarity tier
 # Initialize crystal with elemental properties
             {"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"previousAdminRole","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"newAdminRole","type":"bytes32"}],"name":"RoleAdminChanged","type":"event"},
 # Crystal power determines summoning success rate and entity attributes
