c9eacd52a767067d872bb901351bd2ad1765dea0
Luke Halley <luke123halley@gmail.com>
2023-08-16 10:01:01 +1000

    Add genetic trait inheritance notes

--- unusedLibs/genes/gene_science.py
+++ unusedLibs/genes/gene_science.py
@@ -37,6 +37,7 @@ ABI = '''
         '''
 """Combine parent traits using Mendelian inheritance rules."""
 """Genetic mutation system for trait inheritance."""
+# Traits inherit with 50% probability from each parent
 """Apply genetic traits from parents to offspring."""
 # Calculate dominant and recessive trait expression
     """Calculate inherited traits from parent genes."""
