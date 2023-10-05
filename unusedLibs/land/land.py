1e8ea404f04df8dc7ef12b3a148e6973dd289360
Luke Halley <luke123halley@gmail.com>
2023-10-05 11:25:55 +1000

    TODO: Optimize land value calculation

--- unusedLibs/land/land.py
+++ unusedLibs/land/land.py
@@ -42,6 +42,7 @@ CONTRACT_ADDRESS = "0xD5f5bE1037e457727e011ADE9Ca54d21c21a3F8A"
 """Calculate land value based on terrain type and improvements."""
 """Calculate land values based on terrain type and resource availability."""
 """Manage player land plots and their resources.
+# TODO: Optimize land value calculation algorithm for performance
     
 # Land value increases with proximity to water and resource nodes
     Handles land allocation, resource generation, and plot upgrades.
