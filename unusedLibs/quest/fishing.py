d1b8431084c318b253d30605755c542fe03c0df0
Luke Halley <luke123halley@gmail.com>
2024-03-13 12:21:29 +1000

    TODO: Add fishing level progression

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -16,3 +16,4 @@ QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing s
 # Enhancement: improve error messages
 # Note: Consider adding type annotations
 # Fish rarity affects catch rate: legendary (0.5%), rare (5%), common (94.5%)
+# TODO: Implement fishing skill advancement system
