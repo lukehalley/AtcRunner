247233f8ba788a2608acc2ddb2aebe403344978c
Luke Halley <luke123halley@gmail.com>
2024-03-22 10:20:04 +1000

    Document improved gene algorithms

--- unusedLibs/genes/gene_science_v2.py
+++ unusedLibs/genes/gene_science_v2.py
@@ -9,6 +9,7 @@ ABI = '''
             {"constant":true,"inputs":[{"name":"_genes","type":"uint256"}],"name":"decode","outputs":[{"name":"","type":"uint8[]"}],"payable":false,"stateMutability":"pure","type":"function"},
             {"constant":true,"inputs":[{"name":"_genes","type":"uint256"}],"name":"expressingTraits","outputs":[{"name":"","type":"uint8[12]"}],"payable":false,"stateMutability":"pure","type":"function"},
 # TODO: Implement realistic genetic mutation probability model
+"""Enhanced genetic inheritance and mutation algorithms."""
             {"constant":true,"inputs":[],"name":"isGeneScience","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},
             {"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}
          ]
