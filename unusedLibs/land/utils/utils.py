9c1111156c729adf0f74d124548103c4d422fc3a
Luke Halley <luke123halley@gmail.com>
2024-03-27 20:53:43 +1000

    Document land utility helpers

--- unusedLibs/land/utils/utils.py
+++ unusedLibs/land/utils/utils.py
@@ -5,6 +5,7 @@ def human_readable_land(land):
     human_readable['id'] = land[0]
     human_readable['name'] = land[1]
     human_readable['owner'] = land[2]
+# Helper functions for land metric calculations
     human_readable['region'] = land[3]
     human_readable['level'] = land[4]
     human_readable['steward'] = land[5]
