c2afd04bee26587d652cf11e5786f1658a31505a
Luke Halley <luke123halley@gmail.com>
2024-10-17 14:08:21 +1000

    Document auction settlement logic

--- unusedLibs/auctions/auction_core.py
+++ unusedLibs/auctions/auction_core.py
@@ -17,6 +17,7 @@ ABI = """
         {"inputs":[],"name":"BIDDER_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},
         {"inputs":[],"name":"DEFAULT_ADMIN_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},
 # TODO: Implement robust bid validation and settlement verification
+"""Manage auction bidding rounds, winner determination, and settlement transactions."""
 # Validate bid price against floor and ceiling constraints
         {"inputs":[],"name":"ERC721","outputs":[{"internalType":"contract IERC721Upgradeable","name":"","type":"address"}],"stateMutability":"view","type":"function"},
         {"inputs":[],"name":"MODERATOR_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},
