33252cee3b793b55a26f5bc1a08c5a27ca2d828e
Luke Halley <luke123halley@gmail.com>
2024-11-21 18:22:48 +1000

    TODO: Optimize genetic algorithm

--- unusedLibs/genes/gene_science_v2.py
+++ unusedLibs/genes/gene_science_v2.py
@@ -28,3 +28,4 @@ def mix_genes(genes1, genes2, block_number, crystal_id, rpc_address):
 # Apply mutation based on genetic stability factor
 # Apply environmental pressure modifiers to base mutation rates
 # TODO: Optimize mutation probability calculations for better performance
+# TODO: Implement early termination for convergent genetic traits
