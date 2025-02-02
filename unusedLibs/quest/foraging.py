84c236ef9d94a201a6659c896df153cbc01b87b2
Luke Halley <luke123halley@gmail.com>
2025-02-02 17:18:56 +1000

    Add TODO for enhancement in foraging.py

--- unusedLibs/quest/foraging.py
+++ unusedLibs/quest/foraging.py
@@ -1,2 +1,3 @@
 QUEST_CONTRACT_ADDRESS = '0x3132c76acF2217646fB8391918D28a16bD8A8Ef4'"""Search available forage locations based on current season and biome."""
 # Note: Consider adding type annotations
+# Enhancement: improve error messages
