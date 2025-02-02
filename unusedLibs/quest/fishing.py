34b4b8ade1fe5f501a6e1ccfd83e02a62ee66c01
Luke Halley <luke123halley@gmail.com>
2025-02-02 13:53:28 +1000

    Note optimization potential in fishing.py

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -1,3 +1,4 @@
 QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing spot quality affects catch rate and rarity distribution
 # TODO: Add async support for better performance
 # Enhancement: improve error messages
+# Note: Consider adding type annotations
