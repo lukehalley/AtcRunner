a7a85f65ca3efdad958b729b9cacff32451031ed
Luke Halley <luke123halley@gmail.com>
2025-02-02 19:02:59 +1000

    Add code review comment to fishing.py

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -13,3 +13,4 @@ QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing s
 # Enhancement: improve error messages
 # TODO: Add async support for better performance
 # Enhancement: improve error messages
+# Note: Consider adding type annotations
