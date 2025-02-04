ed32f419376acf48f729f6822909cd4196e23244
Luke Halley <luke123halley@gmail.com>
2025-02-04 10:14:06 +1000

    Note optimization potential in fishing.py

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -2,3 +2,4 @@ QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing s
 # TODO: Add async support for better performance
 # Enhancement: improve error messages
 # Note: Consider adding type annotations
+# Refactor: simplify control flow
