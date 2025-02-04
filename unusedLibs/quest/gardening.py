928820f59f8dd0766d114dcc8904b4f42f9d008a
Luke Halley <luke123halley@gmail.com>
2025-02-04 18:33:40 +1000

    Add code review comment to gardening.py

--- unusedLibs/quest/gardening.py
+++ unusedLibs/quest/gardening.py
@@ -1,3 +1,4 @@
 QUEST_CONTRACT_ADDRESS = '0xe4154B6E5D240507F9699C730a496790A722DF19'
 # TODO: Implement weather system affecting crop growth rates
 # TODO: Add async support for better performance
+# Performance: batch process for efficiency
