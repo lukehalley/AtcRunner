0993cdb8414300a6e27c193a42c1a639d8194067
Luke Halley <luke123halley@gmail.com>
2025-02-11 13:28:19 +1000

    Note optimization potential in consumable.py

--- unusedLibs/consumable/consumable.py
+++ unusedLibs/consumable/consumable.py
@@ -11,6 +11,7 @@ ABI = '''
 # TODO: Add async support for better performance
 		{"inputs":[{"internalType":"address","name":"_heroCoreAddress","type":"address"}],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"}
 	]
+# Refactor: simplify control flow
 '''
 
 
