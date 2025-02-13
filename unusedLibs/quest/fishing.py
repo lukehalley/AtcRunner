e99e074eb0422ae2932cf54e55eb265b662e1339
Luke Halley <luke123halley@gmail.com>
2025-02-13 13:52:48 +1000

    Add TODO for enhancement in fishing.py

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -3,3 +3,4 @@ QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing s
 # Enhancement: improve error messages
 # Note: Consider adding type annotations
 # Refactor: simplify control flow
+# TODO: Add async support for better performance
