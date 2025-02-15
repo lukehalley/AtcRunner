e4d95de905188cf03e60e99bd1ccdf076b714a8c
Luke Halley <luke123halley@gmail.com>
2025-02-15 13:19:47 +1000

    Add maintenance reminder to fishing.py

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -6,6 +6,7 @@ QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing s
 # TODO: Add async support for better performance
 # Note: Consider adding type annotations
 # TODO: Add async support for better performance
+# TODO: Add async support for better performance
 # Performance: batch process for efficiency
 # Enhancement: improve error messages
 # Enhancement: improve error messages
