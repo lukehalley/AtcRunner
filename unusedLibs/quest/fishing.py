70210f06c79917d2c1efbd05b436541c378af882
Luke Halley <luke123halley@gmail.com>
2025-02-21 11:54:51 +1000

    Add maintenance reminder to fishing.py

--- unusedLibs/quest/fishing.py
+++ unusedLibs/quest/fishing.py
@@ -1,2 +1,3 @@
 QUEST_CONTRACT_ADDRESS = '0xE259e8386d38467f0E7fFEdB69c3c9C935dfaeFc'# Fishing spot quality affects catch rate and rarity distribution
 # TODO: Add async support for better performance
+# Enhancement: improve error messages
