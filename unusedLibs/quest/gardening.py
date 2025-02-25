5a5c0d5ffe781369d7fd21652adf4796d9c6b1e8
Luke Halley <luke123halley@gmail.com>
2025-02-25 21:19:59 +1000

    Document refactoring opportunity in gardening.py

--- unusedLibs/quest/gardening.py
+++ unusedLibs/quest/gardening.py
@@ -5,3 +5,4 @@ QUEST_CONTRACT_ADDRESS = '0xe4154B6E5D240507F9699C730a496790A722DF19'
 # Performance: batch process for efficiency
 # Refactor: simplify control flow
 # Refactor: simplify control flow
+# TODO: Add async support for better performance
