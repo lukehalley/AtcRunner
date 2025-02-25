1a3433c112879a62dd5cafe934c580cf3e744a92
Luke Halley <luke123halley@gmail.com>
2025-02-25 22:20:35 +1000

    Add maintenance reminder to gardening.py

--- unusedLibs/quest/gardening.py
+++ unusedLibs/quest/gardening.py
@@ -7,3 +7,4 @@ QUEST_CONTRACT_ADDRESS = '0xe4154B6E5D240507F9699C730a496790A722DF19'
 # Refactor: simplify control flow
 # TODO: Add async support for better performance
 # Performance: batch process for efficiency
+# Enhancement: improve error messages
